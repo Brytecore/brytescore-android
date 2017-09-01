6d0d229a800c354a17f1ed203c4d64ef2be054d7
Emily Morehouse <emily@cuttlesoft.com>
2017-09-01 07:40:44 -0600

    [#150867629 #150867669] Adds devMode toggle and default setting to example app.

--- app/src/main/java/com/brytecore/brytescoreExample/MainActivity.java
+++ app/src/main/java/com/brytecore/brytescoreExample/MainActivity.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.brytecore.Brytescore;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    // ------------------------------------ static variables ------------------------------------ //
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    // ------------------------------------ dynamic variables ----------------------------------- //
+    // Bools for local status of dev and debug mode, used to toggle state with buttons
+    private Boolean devMode = true;
+
 
     // Initialize the API Manager with your API key.
     Brytescore brytescore = new Brytescore("abc123");
@@ -21,9 +28,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Enable dev mode - logs API calls instead of making HTTP request
+        brytescore.devMode(devMode);
+        ToggleButton toggle = ((ToggleButton)findViewById(R.id.devModeButton));
+        toggle.setChecked(devMode);
+
         // Update the API Key label to show our API key for debugging
         final TextView textViewToChange = (TextView) findViewById(R.id.apikey);
         textViewToChange.setText("Your API Key:" + brytescore.getAPIKey());
+    }
+
+    /** Toggle devMode bool, pass to _apiManager, update button title and color */
+    public void toggleDevMode(View view) {
+        devMode = !devMode;
+        brytescore.devMode(devMode);
+
+        ToggleButton toggle = ((ToggleButton)findViewById(R.id.devModeButton));
+        toggle.setChecked(devMode);
     }
 
     /** Called when the user taps the Track Page View button */
