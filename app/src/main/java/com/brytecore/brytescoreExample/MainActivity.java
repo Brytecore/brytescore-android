b05e1938baa166fcbb65fffdf918054c033602f1
Emily Morehouse <emily@cuttlesoft.com>
2017-09-01 07:53:33 -0600

    [#150867627 #150867669] Adds button and tracker to app example for debug mode.

--- app/src/main/java/com/brytecore/brytescoreExample/MainActivity.java
+++ app/src/main/java/com/brytecore/brytescoreExample/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     // ------------------------------------ dynamic variables ----------------------------------- //
     // Bools for local status of dev and debug mode, used to toggle state with buttons
     private Boolean devMode = true;
+    private Boolean debugMode = true;
 
 
     // Initialize the API Manager with your API key.
@@ -30,21 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Enable dev mode - logs API calls instead of making HTTP request
         brytescore.devMode(devMode);
-        ToggleButton toggle = ((ToggleButton)findViewById(R.id.devModeButton));
-        toggle.setChecked(devMode);
+        ToggleButton devModeToggle = ((ToggleButton)findViewById(R.id.devModeButton));
+        devModeToggle.setChecked(devMode);
+
+        // Enable debug mode - turns on console logs
+        brytescore.debugMode(debugMode);
+        ToggleButton debugModeToggle = ((ToggleButton)findViewById(R.id.debugModeButton));
+        debugModeToggle.setChecked(debugMode);
 
         // Update the API Key label to show our API key for debugging
         final TextView textViewToChange = (TextView) findViewById(R.id.apikey);
         textViewToChange.setText("Your API Key:" + brytescore.getAPIKey());
-    }
-
-    /** Toggle devMode bool, pass to _apiManager, update button title and color */
-    public void toggleDevMode(View view) {
-        devMode = !devMode;
-        brytescore.devMode(devMode);
-
-        ToggleButton toggle = ((ToggleButton)findViewById(R.id.devModeButton));
-        toggle.setChecked(devMode);
     }
 
     /** Called when the user taps the Track Page View button */
@@ -53,5 +50,31 @@ public class MainActivity extends AppCompatActivity {
 
         HashMap<String, String> pageViewData = new HashMap<String, String>();
         brytescore.pageView(pageViewData);
+    }
+
+    /** Toggle devMode bool, pass to _apiManager, update button title and color */
+    public void toggleDevMode() {
+        devMode = !devMode;
+        brytescore.devMode(devMode);
+
+        ToggleButton devModeToggle = ((ToggleButton)findViewById(R.id.devModeButton));
+        devModeToggle.setChecked(devMode);
+
+        // If devMode is now on and debugMode was off, debugMode is now on.
+        // Only update if debugMode wasn't already on.
+        if (devMode && ! debugMode) {
+            debugMode = true;
+            ToggleButton debugModeToggle = ((ToggleButton)findViewById(R.id.debugModeButton));
+            debugModeToggle.setChecked(debugMode);
+        }
+    }
+
+    /** Toggle devMode bool, pass to _apiManager, update button title and color */
+    public void toggleDebugMode() {
+        debugMode = !debugMode;
+        brytescore.debugMode(debugMode);
+
+        ToggleButton toggle = ((ToggleButton)findViewById(R.id.debugModeButton));
+        toggle.setChecked(debugMode);
     }
 }
